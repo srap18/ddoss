7843c1d03aaff6265c8ea8fc6c9ac89b5ec7f525
srap18 <48030957+srap18@users.noreply.github.com>
2026-01-21 08:46:11 +0300

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,5 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Start-Process powershell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"") -Verb RunAs
+    Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     exit
 }
 
